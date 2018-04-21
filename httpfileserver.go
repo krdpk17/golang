3a1a048fc17261ddd5c97c0fcab11b57ab2f8d0a
krdpk17 <33544658+krdpk17@users.noreply.github.com>
2018-04-21 15:49:09 +0530

    Try with ' curl -k http://localhost:9999'

---
+++ httpfileserver.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+        "log"
+        "net/http"
+        )
+
+func main(){
+        log.Fatal(http.ListenAndServe(":9999", http.FileServer(http.Dir("/usr/share/doc"))))
+
+}
