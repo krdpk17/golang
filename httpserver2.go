d9a4acecaff6f845fe4fec43a00b6d90398b3da8
krdpk17 <33544658+krdpk17@users.noreply.github.com>
2018-04-21 15:37:05 +0530

    try with 'curl -k http://localhost:9999/two'
    Handled:
    curl -k http://localhost:9999/two
    curl -k http://localhost:9999/one
    
    Not handled:
    curl -k http://localhost:9999/t

---
+++ httpserver2.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+        "fmt"
+        "log"
+        "net/http"
+        )
+
+type helloHandler int
+func (h helloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "you have hit %s\n", r.URL.Path)
+}
+
+func main(){
+        new_mux := http.NewServeMux()
+        new_mux.Handle("/one", helloHandler(1))
+        new_mux.Handle("/two", helloHandler(1))
+        err_closure := http.ListenAndServe(":9999", new_mux)
+        log.Fatal(err_closure)
+
+}
